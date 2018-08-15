b4a68d98203acf940d25a2cdbc2b49badda2f9fd
Effat Chaar <effat.shaar@gmail.com>
2018-08-15 14:47:24 -0700

    added the cases of student stats to student-stats.spec.js

--- __tests__/student-stats.spec.js
+++ __tests__/student-stats.spec.js
@@ -1,6 +1,44 @@
-const stats = require('../lib/student-stats.js')
+const StudentStats = require('../lib/student-stats.js')
 describe('Student Stats', () => {
-    it('works', () => {
-        
+    let statsMachine, processedData;
+    beforeEach(() => {
+        statsMachine = new StudentStats('../lib/data.json')
+        processedData = {
+            projects: {
+                'project1' : {
+                    passed: { number: 10, satisfaction: 10 },
+                    failed: { number: 10, satisfaction: 10 }
+                }
+            },
+            experience: {
+                1: { satisfaction: 10 },
+                2: { satisfaction: 10 },
+                4: { satisfaction: 10 }
+            },
+            demographics: {
+                averageAge: 10,
+                satisfaction: 10
+            }
+        }
+    })
+        it('Should load the JSON', () => {
+            expect(statsMachine.data).toBeDefined()
+            expect(statsMachine.data[0].name).toEqual('Miss Jermain Waters')
+    })
+    describe('Query project query by name', () => {
+        describe('When project name exists', () => {
+            it('should return the correct stats', () => {
+                expect(statsMachine.getProjectByName('project1')).toEqual(
+                    processedData.projects['project1']
+                )
+            })
+        })
+        describe('When project name does not exist', () => {
+            it('should throw an error', () => {
+                expect(() => statsMachine.getProjectByName('aaa')).toThrow(
+                    'invalid project provided'
+                )
+            })
+        }) 
     })
 })
